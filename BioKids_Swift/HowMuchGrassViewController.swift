1d75b47433dd5607234c65d10e9315a8727fad5b
Brandon <bmorton1@gmail.com>
2017-09-26 14:40:13 -0400

    Added new controller for kinds of grass
    Added a new view controller for kinds of grass. This view controller
    also has a cancel button which deletes the observation data and pops
    the view controller back to the root view controller (photo view
    controller).

--- BioKids_Swift/HowMuchGrassViewController.swift
+++ BioKids_Swift/HowMuchGrassViewController.swift
@@ -25,6 +25,31 @@ class HowMuchGrassViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func showMessageToUser(title: String, msg: String)  {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        
+        let yesAction = UIAlertAction(title: "Yes", style: UIAlertActionStyle.destructive) { (result : UIAlertAction) -> Void in
+            // Return
+            print("pressed yes")
+            
+            self.observation = Observation()
+            _ = self.navigationController?.popToRootViewController(animated: true)
+        }
+        
+        let noAction = UIAlertAction(title: "No", style: UIAlertActionStyle.cancel) { (result : UIAlertAction) -> Void in
+            print("pressed no")
+        }
+        
+        alert.addAction(yesAction)
+        alert.addAction(noAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func didPressCancelBtn(_ sender: Any) {
+        self.showMessageToUser(title: "Alert", msg: "You are about to erase this observation. Would you like to delete this observation and return to the Home screen?")
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destination = segue.destination as! NotesViewController
         destination.observation = self.observation
