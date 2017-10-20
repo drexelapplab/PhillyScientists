77874ef40020b2658a4521bb68d7864cb2637ed9
Joseph Baran <jpb358@drexel.edu>
2017-10-20 15:44:12 -0400

    Removed Reachability+Expanded HomeViewController
    Removed reachability and moved functionality to HomeViewController.Swift

--- BioKids_Swift/HomeViewController.swift
+++ BioKids_Swift/HomeViewController.swift
@@ -7,24 +7,54 @@
 //
 
 import UIKit
+import SystemConfiguration
+//func to check if connected to internet(taken from SO)
+func isInternetAvailable() -> Bool
+{
+    var zeroAddress = sockaddr_in()
+    zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+    zeroAddress.sin_family = sa_family_t(AF_INET)
+    let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+            SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+        }
+    }
+    
+    var flags = SCNetworkReachabilityFlags()
+    if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+        return false
+    }
+    let isReachable = flags.contains(.reachable)
+    let needsConnection = flags.contains(.connectionRequired)
+    return (isReachable && !needsConnection)
+}
 
 class HomeViewController: UIViewController {
 
+    var connected=isInternetAvailable()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //hides/shows registration button depending on internet connectivity
+        if(!connected){
+            RegButton.isHidden=true;
+        }
+        else{
+            RegButton.isHidden=false;
+        }
+        
         // Do any additional setup after loading the view.
         HomeLink.addTarget(self, action: "didTapHome", for: .touchUpInside)
     }
     @IBOutlet weak var HomeLink: UIButton!
+    
     //temporarily loads to stackoverflow
     //todo: replace ph link with actual phillyscientists link
+    @IBOutlet weak var RegButton: UIButton!
     @IBAction func didTapHome(sender: AnyObject) {
         UIApplication.shared.openURL(URL(string: "http://www.stackoverflow.com")!)
     }
-    
-    
-    
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
