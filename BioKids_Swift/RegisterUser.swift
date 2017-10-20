75ea30eb636beea224eff3dcb88f67e7fe977c57
Joseph Baran <jpb358@drexel.edu>
2017-10-20 16:49:08 -0400

    Added class for RegisterUser-App breaks
    App breaking, need to restart/clean install

---
+++ BioKids_Swift/RegisterUser.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterUser.swift
+//  BioKids_Swift
+//
+//  Created by Joseph Baran on 10/20/17.
+//  Copyright © 2017 App Lab. All rights reserved.
+//
+
+import UIKit
+
+class RegisterUser: UIViewController {
+    @IBOutlet weak var GroupName: UIPickerView!
+    @IBOutlet weak var GroupPicture: UIPickerView!
+    let pickerData = ["alpha", "bravo", "charlie", "delta"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //GroupName.dataSource = self
+        //GroupName.delegate = self
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+
+}
