c86d34a066217407a5e2489c753b504bba25582d
Joseph Baran <jpb358@drexel.edu>
2017-10-23 16:40:08 -0400

    Fixes
    Fixing Crashes+Fleshed out Nav

--- BioKids_Swift/HomeViewController.swift
+++ BioKids_Swift/HomeViewController.swift
@@ -9,32 +9,14 @@
 import UIKit
 import SystemConfiguration
 //func to check if connected to internet(taken from SO)
-func isInternetAvailable() -> Bool
-{
-    var zeroAddress = sockaddr_in()
-    zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
-    zeroAddress.sin_family = sa_family_t(AF_INET)
-    let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
-            SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
-        }
-    }
-    
-    var flags = SCNetworkReachabilityFlags()
-    if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
-        return false
-    }
-    let isReachable = flags.contains(.reachable)
-    let needsConnection = flags.contains(.connectionRequired)
-    return (isReachable && !needsConnection)
-}
 
 class HomeViewController: UIViewController {
 
-    var connected=isInternetAvailable()
+    var connected = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*
         //hides/shows registration button depending on internet connectivity
         if(!connected){
             RegButton.isHidden=true;
@@ -45,17 +27,40 @@ class HomeViewController: UIViewController {
         
         // Do any additional setup after loading the view.
         HomeLink.addTarget(self, action: "didTapHome", for: .touchUpInside)
+ */
+        connected = isInternetAvailable()
     }
-    @IBOutlet weak var HomeLink: UIButton!
+    
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+    
+    //@IBOutlet weak var HomeLink: UIButton!
     
     //temporarily loads to stackoverflow
     //todo: replace ph link with actual phillyscientists link
-    @IBOutlet weak var RegButton: UIButton!
-    @IBAction func didTapHome(sender: AnyObject) {
+    //@IBOutlet weak var RegButton: UIButton!
+    /*@IBAction func didTapHome(sender: AnyObject) {
         UIApplication.shared.openURL(URL(string: "http://www.stackoverflow.com")!)
     }
 
-    
+    */
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
