e9372ea6213a1d523ca1477075837ab542657a4f
Shiv <ss4328@drexel.edu>
2019-05-23 16:09:54 -0400

    Realm error fix in data submission

--- BioKids_Swift/AppDelegate.swift
+++ BioKids_Swift/AppDelegate.swift
@@ -17,79 +17,79 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let observationContainer = ObservationContainer.sharedInstance
     var window: UIWindow?
     
-//    var groupName = ""
-//    var teacherID = ""
-//    var groupID = ""
-//    var teacher = ""
-//    var locations = [Location]()
-//    let trackerNames = ["Select A Tracker", "Alem", "Aren", "Faraji", "Ghele", "Isoke", "Miniya", "Mkali", "Rakanja", "Sanjo", "Zahra"]
-//    let trackerImgs = ["none", "tracker-alem", "tracker-aren", "tracker-faraji", "tracker-ghele", "tracker-isoke","tracker-miniya", "tracker-mkali", "tracker-rakanja", "tracker-sanjo", "tracker-zahra"]
-//
-//
-//    func parseJSONData(json : JSON, trackerValuePassed : String){
-//        self.groupID = json["groupID"].stringValue
-//        self.teacherID = json["teacherID"].stringValue
-//        self.groupName = json["groupName"].stringValue
-//        self.teacher = json["teacher"].stringValue
-//
-//
-//        let locjson = json["Locations"].arrayValue
-//        for location in locjson {
-//            let locID = Int(location["locationID"].stringValue)
-//            let locName = location["locationName"].stringValue
-//            let locationToAdd = Location(LocationName: locName, LocationID: locID!)
-//            self.locations.append(locationToAdd)
-//            print(locID, locName)
-//        }
-//
-//        self.observationContainer.trackerID = trackerValuePassed
-//        self.observationContainer.groupID = self.groupID
-//        self.observationContainer.groupName = self.groupName
-//        self.observationContainer.teacherID = self.teacherID
-//        self.observationContainer.locations = self.locations
-//
-//    }
-//
-//    func sendAlamofireRequest(submissionURL: URL, parameters: Parameters, chosenTracker: String){
-//        Alamofire.request(submissionURL, method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseString() {
-//            response in
-//            switch response.result {
-//            case .success:
-//                print("Validation Successful...\(String(describing: response.value))")
-//
-//                switch response.value {
-//                case "error_none":
-//                    print("No matching Group Code. If you are having trouble, please go to \nhttps://app.phillyscientists.com")
-//                    break
-//                case "error_tooManyIDs":
-//                    print("Error, please contact developer.")
-//                    break
-//                case "error_noGroupIDReceived":
-//                    print("Try Again.")
-//                    break
-//                default:
-//
-//                    let JSONResponse : JSON = JSON.init(parseJSON: response.result.value!)
-//
-//                    //uncomment this section for debugging
-//                    //                        print("=================<JSON RESP>=================");
-//                    //                        print(JSONResponse)
-//                    //                        print("=================</JSON RESP/>=================");
-//                    //
-//                    self.parseJSONData(json: JSONResponse, trackerValuePassed: chosenTracker)
-//                    //self.saveJSONDataToUserDefaults()
-//                    //self.performSegue(withIdentifier: "groupInfoSegue", sender: self)
-//                    break
-//                }
-//
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-//
+    //    var groupName = ""
+    //    var teacherID = ""
+    //    var groupID = ""
+    //    var teacher = ""
+    //    var locations = [Location]()
+    //    let trackerNames = ["Select A Tracker", "Alem", "Aren", "Faraji", "Ghele", "Isoke", "Miniya", "Mkali", "Rakanja", "Sanjo", "Zahra"]
+    //    let trackerImgs = ["none", "tracker-alem", "tracker-aren", "tracker-faraji", "tracker-ghele", "tracker-isoke","tracker-miniya", "tracker-mkali", "tracker-rakanja", "tracker-sanjo", "tracker-zahra"]
+    //
+    //
+    //    func parseJSONData(json : JSON, trackerValuePassed : String){
+    //        self.groupID = json["groupID"].stringValue
+    //        self.teacherID = json["teacherID"].stringValue
+    //        self.groupName = json["groupName"].stringValue
+    //        self.teacher = json["teacher"].stringValue
+    //
+    //
+    //        let locjson = json["Locations"].arrayValue
+    //        for location in locjson {
+    //            let locID = Int(location["locationID"].stringValue)
+    //            let locName = location["locationName"].stringValue
+    //            let locationToAdd = Location(LocationName: locName, LocationID: locID!)
+    //            self.locations.append(locationToAdd)
+    //            print(locID, locName)
+    //        }
+    //
+    //        self.observationContainer.trackerID = trackerValuePassed
+    //        self.observationContainer.groupID = self.groupID
+    //        self.observationContainer.groupName = self.groupName
+    //        self.observationContainer.teacherID = self.teacherID
+    //        self.observationContainer.locations = self.locations
+    //
+    //    }
+    //
+    //    func sendAlamofireRequest(submissionURL: URL, parameters: Parameters, chosenTracker: String){
+    //        Alamofire.request(submissionURL, method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseString() {
+    //            response in
+    //            switch response.result {
+    //            case .success:
+    //                print("Validation Successful...\(String(describing: response.value))")
+    //
+    //                switch response.value {
+    //                case "error_none":
+    //                    print("No matching Group Code. If you are having trouble, please go to \nhttps://app.phillyscientists.com")
+    //                    break
+    //                case "error_tooManyIDs":
+    //                    print("Error, please contact developer.")
+    //                    break
+    //                case "error_noGroupIDReceived":
+    //                    print("Try Again.")
+    //                    break
+    //                default:
+    //
+    //                    let JSONResponse : JSON = JSON.init(parseJSON: response.result.value!)
+    //
+    //                    //uncomment this section for debugging
+    //                    //                        print("=================<JSON RESP>=================");
+    //                    //                        print(JSONResponse)
+    //                    //                        print("=================</JSON RESP/>=================");
+    //                    //
+    //                    self.parseJSONData(json: JSONResponse, trackerValuePassed: chosenTracker)
+    //                    //self.saveJSONDataToUserDefaults()
+    //                    //self.performSegue(withIdentifier: "groupInfoSegue", sender: self)
+    //                    break
+    //                }
+    //
+    //            case .failure(let error):
+    //                print(error)
+    //            }
+    //        }
+    //    }
+    //
     
-    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         // Update Realm Schemas if necessary
@@ -143,8 +143,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         // Change the font size of the Tab Bar Controller
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Montserrat", size: 18)!], for: .normal)
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Montserrat", size: 18)!], for: .selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font: UIFont(name: "Montserrat", size: 18)!], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font: UIFont(name: "Montserrat", size: 18)!], for: .selected)
         
         
         
@@ -154,10 +154,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         for (key, value) in UserDefaults.standard.dictionaryRepresentation() {
             print("\(key) = \(value) \n")
         }
-
+        
         print("UserDefaults Values========================")
         
-
+        
         // Then push that view controller onto the navigation stack
         if UserDefaults.standard.bool(forKey: "loggedIn"){
             print("logged in already!")
@@ -167,16 +167,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 if UserDefaults.standard.string(forKey: "trackerID") != nil{
                     observationContainer.trackerID = UserDefaults.standard.string(forKey: "trackerID")!
                     
-//                    let submissionURL = URL(string: "https://app.phillyscientists.com/verifyGroupDev.php");
-//
-//                    let parameters: Parameters = ["uniqueCode": observationContainer.groupID,
-//                                                  "trackerID": observationContainer.trackerID
-//                    ]
-//                    self.sendAlamofireRequest(submissionURL: submissionURL!, parameters: parameters, chosenTracker: observationContainer.trackerID)
+                    //                    let submissionURL = URL(string: "https://app.phillyscientists.com/verifyGroupDev.php");
+                    //
+                    //                    let parameters: Parameters = ["uniqueCode": observationContainer.groupID,
+                    //                                                  "trackerID": observationContainer.trackerID
+                    //                    ]
+                    //                    self.sendAlamofireRequest(submissionURL: submissionURL!, parameters: parameters, chosenTracker: observationContainer.trackerID)
                     
                     
                     observationContainer.populateObservationContainerInstance(groupCode: UserDefaults.standard.string(forKey: "groupCode")!, chosenTrackerString: UserDefaults.standard.string(forKey: "trackerID")!)
-
+                    
                 }
             }
             let viewController: UITabBarController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
@@ -187,52 +187,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         else{
             print("Not logged in currently")
         }
-//        else{
-//            print("not logged in already!")
-//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//            if UserDefaults.standard.string(forKey: "groupName") != nil{
-//                observationContainer.groupID = UserDefaults.standard.string(forKey: "groupID")!
-//
-//                if UserDefaults.standard.string(forKey: "trackerID") != nil{
-//                    observationContainer.trackerID = UserDefaults.standard.string(forKey: "trackerID")!
-//                    observationContainer.populateObservationContainerInstance(groupCode: UserDefaults.standard.string(forKey: "groupName")!, chosenTrackerString: UserDefaults.standard.string(forKey: "trackerID")!)
-//                }
-//            }
-//
-//
-//            let viewController: UITabBarController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
-//
-//            window?.rootViewController = viewController
-//            window?.makeKeyAndVisible()
-//        }
+        //        else{
+        //            print("not logged in already!")
+        //            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        //            if UserDefaults.standard.string(forKey: "groupName") != nil{
+        //                observationContainer.groupID = UserDefaults.standard.string(forKey: "groupID")!
+        //
+        //                if UserDefaults.standard.string(forKey: "trackerID") != nil{
+        //                    observationContainer.trackerID = UserDefaults.standard.string(forKey: "trackerID")!
+        //                    observationContainer.populateObservationContainerInstance(groupCode: UserDefaults.standard.string(forKey: "groupName")!, chosenTrackerString: UserDefaults.standard.string(forKey: "trackerID")!)
+        //                }
+        //            }
+        //
+        //
+        //            let viewController: UITabBarController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+        //
+        //            window?.rootViewController = viewController
+        //            window?.makeKeyAndVisible()
+        //        }
         
         
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-
+    
 }
+
 
